b43fc8438b684ef481eb051650152b381784c8a9
dide0001 <dide0001@stud.kea.dk>
2026-05-15 12:40:00 +0200

    Add Webplayer component and update Playground layout

--- src/app/playground/page.js
+++ src/app/playground/page.js
@@ -4,18 +4,22 @@ import LatestVideo from "@/components/LatestVideo";
 import FeauteredEvents from "@/components/FeauteredEvents";
 import PersonSpotlight from "@/components/PersonSpotlight";
 import Newsletter from "@/components/Newsletter";
+import Webplayer from "@/components/Webplayer";
+
 
 export default function Playground() {
   return (
     <div className="bg-(--color-bg) text-white mb-12">
-        <H2Normal title="Welcome in Nightclub" bgImage="/assets/bg/footerbg.jpg" />
+      <Webplayer/>
+        {/* <H2Normal title="Welcome in Nightclub" bgImage="/assets/bg/footerbg.jpg" />
       <WelcomeInNightclub />
         <H2Normal title="Latest Video" />
       <LatestVideo />
         <H2Normal title="Feautered Events" />
         <FeauteredEvents />
+        <Webplayer src="/assets/audio/sample.mp3" title="Sample Song" artist="Sample Artist" />
         <PersonSpotlight />
-        <Newsletter />
+        <Newsletter /> */}
     </div>
   );
 }
